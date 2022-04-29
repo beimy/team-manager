15e45294dc21471dae7e9707d720fc8042f16d4c
Beimy <arbm159357@gmail.com>
2022-04-29 18:34:26 -0400

    created manager class and tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require('../lib/Manager');
+
+test('creates a new manager', () => {
+    const manager = new Manager('Manager', 'manager@somewhere.com', 15, 201);
+
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+})
+
+test('checks manager methods', () => {
+    const manager = new Manager('Manager', 'manager@somewhere.com');
+
+    expect(manager.getName()).toBe('Manager');
+    expect(manager.getId()).toEqual(expect.any(Number));
+    expect(manager.getEmail()).toBe('manager@somewhere.com');
+    expect(manager.getRole()).toBe('Manager');
+    expect(manager.getOfficeNumber()).toEqual(expect.any(Number));
+})
+
